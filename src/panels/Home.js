e860b454e6d6a3b40a4bd2e24be7971284799204
EMPIRE-DUCK <62853075+EMPIRE-DUCK@users.noreply.github.com>
2020-06-03 20:49:29 +0500

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -10,7 +10,7 @@ import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
 
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
-		<PanelHeader>Example</PanelHeader>
+		<PanelHeader>Example for test </PanelHeader>
 		{fetchedUser &&
 		<Group title="User Data Fetched with VK Bridge">
 			<Cell
@@ -25,7 +25,7 @@ const Home = ({ id, go, fetchedUser }) => (
 		<Group title="Navigation Example">
 			<Div>
 				<Button size="xl" level="2" onClick={go} data-to="persik">
-					Show me the Persik, please
+					Secret information
 				</Button>
 			</Div>
 		</Group>
