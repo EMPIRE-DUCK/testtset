db037b5540514fcde7e4e702a55bed9ea4c7deb3
EMPIRE-DUCK <62853075+EMPIRE-DUCK@users.noreply.github.com>
2020-06-13 22:40:18 +0500

    Update Persik.js

--- src/panels/Persik.js
+++ src/panels/Persik.js
@@ -7,6 +7,7 @@ import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
 import Button from '@vkontakte/vkui/dist/components/Button/Button';
+import Progress from '@vkontakte/vkui/dist/components/Progress/Progress';
 
 import persik from '../img/persik.png';
 import './Persik.css';
