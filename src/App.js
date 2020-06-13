d80b441ae40b0e1693c95e52ae3398c4ab50e03e
EMPIRE-DUCK <62853075+EMPIRE-DUCK@users.noreply.github.com>
2020-06-13 23:06:23 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import bridge from '@vkontakte/vk-bridge';
 import View from '@vkontakte/vkui/dist/components/View/View';
 import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
+import Button from '@vkontakte/vkui/dist/components/Button/Button';
 import '@vkontakte/vkui/dist/vkui.css';
 
 import Home from './panels/Home';
